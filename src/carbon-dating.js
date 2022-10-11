3fc896401a82f7be68b22721f1a05be8cf706a82
Aleksander Chumachenko <ledich19@gmail.com>
2022-10-11 23:34:49 +0300

    feat: ansver carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+if (!isNaN(parseFloat(sampleActivity)) && isFinite(sampleActivity) && typeof sampleActivity === 'string' && sampleActivity > 0 && sampleActivity <= 15) {
+  
+  const k = 0.693 / HALF_LIFE_PERIOD
+  const log = Math.log( MODERN_ACTIVITY / sampleActivity )
+  
+  console.log(Math.ceil(log  / k ));
+  return Math.ceil(log  / k )
 }
-
+return false
+}
+dateSample('1')
 module.exports = {
   dateSample
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -18,7 +18,6 @@ function countCats(matrix) {
   const result = matrix.flat().filter((e) => /(\^\^)/.test(e) && e.length === 2)
  return result ? result.length : 0
 }
-
 module.exports = {
   countCats
 };
