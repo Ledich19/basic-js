758d7f7e5dc2a8ee3a17f9943f93b97c6d89282d
Aleksander Chumachenko <ledich19@gmail.com>
2022-10-11 23:40:09 +0300

    refactor: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,29 +6,31 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
 function dateSample(sampleActivity) {
-if (!isNaN(parseFloat(sampleActivity)) && isFinite(sampleActivity) && typeof sampleActivity === 'string' && sampleActivity > 0 && sampleActivity <= 15) {
-  
-  const k = 0.693 / HALF_LIFE_PERIOD
-  const log = Math.log( MODERN_ACTIVITY / sampleActivity )
-  
-  console.log(Math.ceil(log  / k ));
-  return Math.ceil(log  / k )
+  const checkNumber =
+    !isNaN(parseFloat(sampleActivity)) && isFinite(sampleActivity);
+  const checkValue = sampleActivity > 0 && sampleActivity <= 15;
+  const checkType = typeof sampleActivity === "string";
+
+  if (checkNumber && checkType && checkValue) {
+    return Math.ceil(
+      Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
+    );
+  }
+  return false;
 }
-return false
-}
-dateSample('1')
+
 module.exports = {
-  dateSample
+  dateSample,
 };
