71b5a75f6a0b09897987b5c506f089a3a0c279da
Aleksandr <ledich19@gmail.com>
2022-10-15 22:31:38 +0300

    feat: add solution mine sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const answer = [];
+  for (let i = 0; i < matrix.length; i++) {
+    answer.push([])
+    for (let j = 0; j < matrix[i].length; j++) {
+      const top = i === 0 ? 0 : matrix[i - 1][j] ? 1 : 0;
+      const topRight = (i === 0 || j === (matrix[i].length - 1)) ? 0 : matrix[i - 1][j + 1] ? 1 : 0;
+      const topRLeft = (i === 0 || j === 0) ? 0 : matrix[i - 1][j - 1] ? 1 : 0;
+      const bottom = (i === matrix.length - 1) ? 0 : matrix[i + 1][j] ? 1 : 0;
+      const bottomRight = ((i === matrix.length - 1) || j === matrix[i].length - 1) ? 0 : matrix[i + 1][j + 1] ? 1 : 0;
+      const bottomLeft = ((i === matrix.length - 1) || 0) ? 0 : matrix[i + 1][j - 1] ? 1 : 0;
+      const right = (j === matrix[i].length - 1) ? 0 : matrix[i][j + 1] ? 1 : 0;
+      const left = j === 0 ? 0 : matrix[i][j - 1] ? 1 : 0;
+      answer[i][j] = top + bottom + left + right + topRight + topRLeft + bottomLeft + bottomRight;
+    }
+  }
+return answer
   // remove line with error and write your code here
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
